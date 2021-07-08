caabb0446e5774a677fd62688fc55d5d5ee13b2a
dylan <dylan_zeng92@qq.com>
2021-07-08 10:29:29 +0800

    add nock default replay headers

--- src/sardine.ts
+++ src/sardine.ts
@@ -1,8 +1,13 @@
+import nock from 'nock/types'
+import { Swagger } from './swagger'
+
 interface SardineOptions {
   url: string
   defaultFakeConfigs?: Record<string, any>
   // TODO: nock config
 }
 export class Sardine {
+  public scope: nock.Scope | undefined
+  public swagger: Swagger | undefined
   constructor(options: SardineOptions) {}
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -7,5 +7,10 @@ export function initNock(swagger: Swagger): nock.Scope {
   // @ts-ignore
   const schemes: Schemes = swagger.schemes ? swagger.schemes[0] : 'http'
   const basePath: string = schemes + '://' + swagger.host + swagger.basePath
-  return Nock(basePath)
+  // https://github.com/nock/nock#default-reply-headers
+  return Nock(basePath).defaultReplyHeaders({
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Origin': swagger.host
+  })
 }
