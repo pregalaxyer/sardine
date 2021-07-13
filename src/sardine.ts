865fd37690f63bf699338be94c514ee236b382b3
dylan <dylan_zeng92@qq.com>
2021-07-13 18:22:07 +0800

    feat: add sardine class

--- src/sardine.ts
+++ src/sardine.ts
@@ -1,11 +1,34 @@
 import { Swagger } from './swagger'
+import Koa, { Middleware } from 'koa'
+import { getSwaggerJsonFromUrl } from './share'
+import { initKoa } from './server'
 
 interface SardineOptions {
   url: string
   defaultFakeConfigs?: Record<string, any>
+  koaMiddleware?: Middleware
   // TODO: nock config
 }
-export class Sardine {
+export default class Sardine {
+  public koa: Koa | undefined
   public swagger: Swagger | undefined
-  constructor(options: SardineOptions) {}
+  public url: SardineOptions['url']
+  public defaultFakeConfigs: SardineOptions['defaultFakeConfigs']
+  public koaMiddleware: SardineOptions['koaMiddleware']
+
+  constructor(options: SardineOptions) {
+    this.url = options.url
+    //TODO default fake config
+    this.defaultFakeConfigs = options.defaultFakeConfigs || {}
+    this.koaMiddleware = options.koaMiddleware
+    this.init()
+  }
+  /**
+   * get swagger json and create koa server
+   */
+  init = async () => {
+    this.swagger = await getSwaggerJsonFromUrl(this.url)
+    this.koa = initKoa(this.swagger)
+    this.koaMiddleware && this.koa.use(this.koaMiddleware)
+  }
 }
--- test/sardine.test.ts
+++ test/sardine.test.ts
@@ -1,18 +1,6 @@
-import { Sardine } from '../src/sardine'
+import Sardine from '../src/sardine'
 
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
-
-  it('Sardine is instantiable', () => {
-    expect(
-      new Sardine({
-        url: ''
-      })
-    ).toBeInstanceOf(Sardine)
-  })
-})
+describe('Dummy test', () => {})
